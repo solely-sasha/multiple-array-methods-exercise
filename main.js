f180b2df3c149bb695375b4cc4dc84267d287a90
sasha sims <solely.sashasims@gmail.com>
2023-01-24 04:54:52 -0500

    finished

--- main.js
+++ main.js
@@ -45,20 +45,40 @@ var peopleArray = [
 
 
 
-function sortedOfAge(arr){
-    // Your code here
- }
- 
- console.log(sortedOfAge(peopleArray));
- 
- /*
- Output:
- [
-     "<li>Kyle Mooney is 27</li>",
-     "<li>Sarah Palin is 47</li>",
-     "<li>Rick Sanchez is 78</li>",
-     "<li>Morty Smith is 29</li>",
-     "<li>Lev Tolstoy is 82</li>"
- ]
- */
- 
+function sortedOfAge(arr) {
+    const ofAge = arr.filter(person => person.age > 18)
+
+    const alphabetically = ofAge.sort((a, b) => {
+        let nameA = a.lastName.toUpperCase()
+        let nameB = b.lastName.toUpperCase()
+        if (nameA < nameB) {
+            return -1;
+        }
+        if (nameA > nameB) {
+            return 1;
+        }
+        return 0;
+    })
+
+
+
+
+    const sortedAges = alphabetically.map(obj => `<li>${obj.firstName} ${obj.lastName} is ${obj.age} </li>`)
+
+    return sortedAges
+}
+
+
+
+console.log(sortedOfAge(peopleArray));
+
+/*
+Output:
+[
+    "<li>Kyle Mooney is 27</li>",
+    "<li>Sarah Palin is 47</li>",
+    "<li>Rick Sanchez is 78</li>",
+    "<li>Morty Smith is 29</li>",
+    "<li>Lev Tolstoy is 82</li>"
+]
+*/
